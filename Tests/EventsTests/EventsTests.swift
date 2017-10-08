7a4bace55f751038492b9d7b64028d29847241e3
robinwalterfit <ask@robinwalterfit.com>
2017-10-08 16:24:05 +0200

    Added Tests

--- Tests/EventsTests/EventsTests.swift
+++ Tests/EventsTests/EventsTests.swift
@@ -2,15 +2,66 @@ import XCTest
 @testable import Events
 
 class EventsTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Events().text, "Hello, World!")
+    
+    func testPlay() {
+        
+        let zeus = CatTest()
+        let stephen = HumanTest()
+        
+        stephen.testAdoptCat( cat: zeus )
+        zeus.testMeow()
+        
     }
+    
+}
 
+class CatTest: XCTestCase {
+    
+    var eventManager = EventManager()
+    
+    func testMeow() {
+        
+        print( "Cat: MRaawwweeee" )
+        
+        self.eventManager.trigger( tag: "meow", param: "The cat is hungry!" )
+        
+    }
+    
+}
 
-    static var allTests = [
-        ("testExample", testExample),
-    ]
+class HumanTest: XCTestCase {
+    
+    func testAdoptCat( cat: CatTest ) {
+        
+        let _ = cat.eventManager.addListener( tag: "meow", action: {
+            
+            print( "Human: Awww, what a cute kitty *pets cat*" )
+            
+        } )
+        
+        let _ = cat.eventManager.addListener( tag: "meow", action: self.testDayDream, priority: 0 )
+        
+        let _ = cat.eventManager.addListener( tag: "meow", action: self.testPonderCat )
+        
+    }
+    
+    func testDayDream() {
+        
+        print( "Human daydreams about owning a dog" )
+        
+    }
+    
+    func testPonderCat( param: Any? ) {
+        
+        if let info = param as? String {
+            
+            print( "Oooh, I think I know:" )
+            
+            print( info )
+            
+        }
+        
+        
+    }
+    
 }
